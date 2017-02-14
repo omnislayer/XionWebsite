1a4a9bcb9118057ca917dfb69ceb6b81c82a7bea
omnislayer <omnislayer5@gmail.com>
2017-02-14 21:45:06 +0800

    Version 0.2
    Added slider

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
