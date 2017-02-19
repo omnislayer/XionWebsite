3b15fdd757306a90964cfc1378ecfdeb6cc5cb79
omnislayer <omnislayer5@gmail.com>
2017-02-19 21:14:01 +0800

    Version 0.5
    Fixed Scrolling and Fonts

--- js/init.js
+++ js/init.js
@@ -4,12 +4,14 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     //$('.scrollspy').scrollSpy(); // SCROLLSPY PROBLEM!
-    // $('.scrollspy').scrollSpy({
-		// scrollOffset: $(window).height();
-	// });
+    $('.scrollspy').scrollSpy({scrollOffset:0});
 	// $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
 	$(".lazy").lazyload(); // Lazyload
 	$('.modal').modal();
+	
+	$('body').data().scrollspy.options.offset = offsetValue;
+	// force scrollspy to recalculate the offsets to your targets
+	$('body').data().scrollspy.process();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
